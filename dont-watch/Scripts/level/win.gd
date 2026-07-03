d08796a1ae5ef32104c7089d58fb666a07d0387a
Zonnap <Zonnap@outlook.com>
2026-07-03 19:35:36 -0400

    EXPORT

--- dont-watch/Scripts/level/win.gd
+++ dont-watch/Scripts/level/win.gd
@@ -1,5 +1,5 @@
 extends Node3D
-@onready var ControlButton = $Control/Button
+@onready var ControlButton = $Control/Replay
 @onready var CompleteAnimation = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,5 +10,5 @@ func _process(delta: float) -> void:
 	if CompleteAnimation.is_playing() == false:
 		ControlButton.visible = true
 
-func _on_button_pressed() -> void:
+func _on_replay_pressed() -> void:
 	get_tree().change_scene_to_file.call_deferred("res://Scenes/Levels/level_1.tscn")
