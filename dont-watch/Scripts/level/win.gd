a45dbea37bf1c2a493bf011e824b6e8f877a7bf8
Deltazoe <138903048+Deltazoe@users.noreply.github.com>
2026-07-03 22:41:41 -0400

    sauce for win and main menu

--- dont-watch/Scripts/level/win.gd
+++ dont-watch/Scripts/level/win.gd
@@ -1,14 +1,5 @@
 extends Node3D
 @onready var ControlButton = $Control/Replay
-@onready var CompleteAnimation = $AnimationPlayer
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	ControlButton.visible = false
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if CompleteAnimation.is_playing() == false:
-		ControlButton.visible = true
 
 func _on_replay_pressed() -> void:
 	get_tree().change_scene_to_file.call_deferred("res://Scenes/Levels/level_1.tscn")
