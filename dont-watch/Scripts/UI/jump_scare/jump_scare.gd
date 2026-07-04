01fa5969b7fc214f349a9bfd6f71a9547042234f
Zonnap <Zonnap@outlook.com>
2026-07-03 21:01:48 -0400

    Export x2

--- dont-watch/Scripts/UI/jump_scare/jump_scare.gd
+++ dont-watch/Scripts/UI/jump_scare/jump_scare.gd
@@ -1,4 +1,5 @@
 extends Node3D
-
+@onready var Anim_play = $AnimationPlayer
 func _on_button_pressed() -> void:
+	Anim_play.stop()
 	get_tree().change_scene_to_file.call_deferred("res://Scenes/Levels/level_1.tscn")
